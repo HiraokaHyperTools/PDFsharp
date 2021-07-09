cef81b3f2570a49aa70abcade49bcdc1034e8c16
kenjiuno <ku@digitaldolphins.jp>
2021-07-09 22:49:47 +0900

    - Add missing one.

---
+++ PDFsharp/code/PdfSharp.Xps/PdfSharp.Xps.Rendering/PathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdfSharp.Xps.Rendering
+{
+  static class PathHelper
+  {
+    internal static string Combine(string path1, string path2)
+    {
+      if (path2.StartsWith("/"))
+      {
+        return path2;
+      }
+      return path1 + path2;
+    }
+  }
+}
