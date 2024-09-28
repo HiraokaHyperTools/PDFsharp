4d72937a5be16d726cd0fbf58502453f75efd20e
kenjiuno <ku@digitaldolphins.jp>
2024-09-28 19:33:33 +0900

    Update unit test

--- PDFsharp/testing/PdfSharp.Xps.UnitTests/Render2/XpsToPdfToPngAndCompare.cs
+++ PDFsharp/testing/PdfSharp.Xps.UnitTests/Render2/XpsToPdfToPngAndCompare.cs
@@ -18,6 +18,7 @@ using PdfSharp.Xps.Rendering;
 using IOPath = System.IO.Path;
 using Path = System.IO.Path;
 using System.Security.Cryptography;
+using System.Linq;
 
 namespace PdfSharp.Xps.UnitTests.Render2
 {
@@ -42,6 +43,61 @@ namespace PdfSharp.Xps.UnitTests.Render2
 
     [Test]
     [DeploymentItemFrom("@PdfSharp.Xps.UnitTests/Render2/xps", "xps")]
+    [TestCase("ImageBrushTileMode.xps")]
+    [Ignore("TDD")]
+    public void TestRasterizationAndThenLaunchViewer(string xpsInput)
+    {
+      string dir = TestContext.WorkDirectory;
+
+      Directory.CreateDirectory(Path.Combine(dir, "pdf"));
+      Directory.CreateDirectory(Path.Combine(dir, "png"));
+
+      string pdftoppm = Path.Combine(dir, "tools/win32/pdftoppm.exe");
+
+      string xpsFile = Path.Combine(dir, "xps", xpsInput);
+
+      string pdfFile = Path.Combine(
+        Path.GetDirectoryName(
+          Path.GetDirectoryName(xpsFile)
+        ),
+        "pdf",
+        Path.GetFileNameWithoutExtension(xpsFile) + ".pdf"
+      );
+
+      XpsConverter.Convert(xpsFile, pdfFile, 0);
+
+      string pngPrefix = Path.Combine(
+        Path.GetDirectoryName(
+          Path.GetDirectoryName(xpsFile)
+        ),
+        "png",
+        Path.GetFileNameWithoutExtension(xpsFile)
+      );
+
+      ProcessStartInfo psi = new ProcessStartInfo(
+        pdftoppm,
+        $"-png -r 150 \"{pdfFile}\" \"{pngPrefix}\""
+      );
+      psi.UseShellExecute = false;
+
+      Process process = Process.Start(
+        psi
+      );
+
+      process.WaitForExit();
+
+      if (process.ExitCode != 0)
+      {
+        throw new Exception($"pdftoppm exit code {process.ExitCode} seems to be error.");
+      }
+
+      Directory.GetFiles(Path.GetDirectoryName(pngPrefix), Path.GetFileName(pngPrefix) + "*")
+        .ToList()
+        .ForEach(one => Process.Start(new ProcessStartInfo(one) { UseShellExecute = true }));
+    }
+
+    [Test]
+    [DeploymentItemFrom("@PdfSharp.Xps.UnitTests/Render2/xps", "xps")]
     [DeploymentItemFrom("@PdfSharp.Xps.UnitTests/Render2/png-reference", "png-reference")]
     [DeploymentItemFrom("@PdfSharp.Xps.UnitTests/Render2/tools", "tools")]
     public void TestRegressionByRasterization()
@@ -52,7 +108,7 @@ namespace PdfSharp.Xps.UnitTests.Render2
       Directory.CreateDirectory(Path.Combine(dir, "png"));
 
       string pdftoppm = Path.Combine(dir, "tools/win32/pdftoppm.exe");
-      
+
       string[] xpsFiles = Directory.GetFiles(dir, "xps/*.xps");
 
       if (xpsFiles.Length == 0)
@@ -98,7 +154,7 @@ namespace PdfSharp.Xps.UnitTests.Render2
 
       string pngDir = Path.Combine(dir, "png");
 
-      var hash = new SHA512Managed();
+      var hash = SHA512.Create();
 
       foreach (string pngFile in Directory.GetFiles(pngDir))
       {
